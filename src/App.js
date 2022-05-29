9ac9d589b01d23bff93e0d88e26faca3a98bbee0
Sumati <sumatijena21997@gmail.com>
2022-05-29 19:02:39 +0530

    infinite scroll implemented

--- src/App.js
+++ src/App.js
@@ -15,15 +15,14 @@ export default class app extends Component{
       <div>
       <NavBar/>
       <Routes>
-      <Route exact path="/"  element={<News key='general' category={'general'}/>}/>
-      <Route exact path="/business"   element={<News key='business' category={'business'}/>}/>
-      <Route exact path="/entertainment"  element={<News key='entertainment' category={'entertainment'}/>}/>
-      <Route exact path="/health"  element={<News key='health' category={'health'}/>}/>
-      <Route exact path="/technology"  element={<News key='technology' category={'technology'}/>}/>
-      <Route exact path="/science"  element={<News key='science' category={'science'}/>}/>
-      <Route exact path="/sports"  element={<News key='sports' category={'sports'}/>}/>
+        <Route exact path="/"  element={<News key='general' category={'general'}/>}/>
+        <Route exact path="/business"   element={<News key='business' category={'business'}/>}/>
+        <Route exact path="/entertainment"  element={<News key='entertainment' category={'entertainment'}/>}/>
+        <Route exact path="/health"  element={<News key='health' category={'health'}/>}/>
+        <Route exact path="/technology"  element={<News key='technology' category={'technology'}/>}/>
+        <Route exact path="/science"  element={<News key='science' category={'science'}/>}/>
+        <Route exact path="/sports"  element={<News key='sports' category={'sports'}/>}/>
       </Routes>
-      
       </div>
       </BrowserRouter>
     )
--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Spineer from "./Spineer.js";
 import NewsItem from "../NewsItem";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
   static propTypes = {
@@ -14,8 +15,9 @@ export default class News extends Component {
     super(props);
     this.state = {
       articles: [],
-      loading: true,
+      loading: false,
       page: 1,
+      totalResults:0
     };
     document.title=`${this.Capitalize(this.props.category)}-DailyNews`;
   }
@@ -33,66 +35,56 @@ export default class News extends Component {
   async componentDidMount() {
     this.updateDate();
   }
-  handlePrvClick = async () => {
+   fetchMoreData= async () =>{
+    this.setState({page:this.state.page+1});
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=960be6c2fa9a414796074732a052dc22&page=${this.state.page}&pageSize=20`;
+    this.setState({ loading: true });
+    const date = await fetch(url);
+    const fetchDate = await date.json();
     this.setState({
-      page: this.state.page - 1,
+      articles: this.state.articles.concat(fetchDate.articles),
+      totalResults: 60,
+      loading: false
     });
-    this.updateDate();
-  };
-  handleNextClick = async () => {
-    this.setState({
-      page: this.state.page + 1,
-    });
-    this.updateDate();
-  };
+    console.log(this.state.articles.length);
+    console.log(this.state.totalResults);
+  }
   render() {
     return (
       <>
-        <div className="container mt-4">
-          <h1>News-Monkey Top HeadLine {this.Capitalize(this.props.category)}</h1>
-          {this.state.loading && <Spineer />}
-          <div className="row">
-            {!this.state.loading &&
-              this.state.articles.map((element) => {
-                return (
-                  <div className="col-md-4  my-4" key={element.url}>
-                    <NewsItem
-                      title={element.title ? element.title.slice(0, 20) : ""}
-                      description={
-                        element.description
-                          ? element.description.slice(0, 45)
-                          : ""
-                      }
-                      date={element.publishedAt}
-                      author={element.author ? element.author : "unknown"}
-                      image={element.urlToImage ? element.urlToImage : ""}
-                      url={element.url}
-                    />
-                  </div>
-                );
-              })}
-          </div>
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button
-            type="button"
-            className="btn btn-dark"
-            disabled={this.state.page <= 1}
-            onClick={this.handlePrvClick}
+          <h1 className="my-4 text-center">News-Monkey Top HeadLine {this.Capitalize(this.props.category)}</h1>
+      {this.state.loading && <Spineer />}
+          <InfiniteScroll
+            dataLength={this.state.articles.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.articles.length!==this.state.totalResults}
+            loader={<Spineer/>}
           >
-            &larr;Previous
-          </button>
-          <button
-            type="button"
-            className="btn btn-dark"
-            disabled={
-              this.state.page + 1 > Math.ceil(this.state.totalResults / 20)
-            }
-            onClick={this.handleNextClick}
-          >
-            Next &rarr;
-          </button>
-        </div>
+          <div className="container">
+            <div className="row">
+              {
+                this.state.articles.map((element) => {
+                  return (
+                    <div className="col-md-4  my-4" key={element.url}>
+                      <NewsItem
+                        title={element.title ? element.title.slice(0, 20) : ""}
+                        description={
+                          element.description
+                            ? element.description.slice(0, 45)
+                            : ""
+                        }
+                        date={element.publishedAt}
+                        author={element.author ? element.author : "unknown"}
+                        image={element.urlToImage ? element.urlToImage : ""}
+                        url={element.url}
+                      />
+                    </div>
+                  );
+                })}
+            </div>
+            </div>
+          </InfiniteScroll>
+         
       </>
     );
   }
